8f11c16b3f133c80f6a4a7f3c397fbe73c068ae6
AkashSuchak <suchak.akash@gmail.com>
2020-10-18 16:15:10 +0530

    Added : Triplet Combinations

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #Author : Akash Suchak
-#As A Simulator, Loop to crete doublet combination. Store the doublet Combination as well as the percentage
+#As A Simulator, Do the same Triplet Combination
 
 
 #User-Input
@@ -9,11 +9,8 @@ read -p "Enter Number : " number
 
 #Declaring Dictionary
 declare -A coin
+declare -A display
 
-hh=0
-ht=0
-th=0
-tt=0
 
 if [[ $number ]] && [ "$number" -eq "$number" 2>/dev/null -a  "$number" -gt 0 ]; then
 
@@ -23,42 +20,54 @@ if [[ $number ]] && [ "$number" -eq "$number" 2>/dev/null -a  "$number" -gt 0 ];
 		#Random Value 0 or 1
 		coin1=$((RANDOM%2))
 		coin2=$((RANDOM%2))
+		coin3=$((RANDOM%2))
 
-		#Checking Doublet
-		if [ "$coin1" -eq 0 -a "$coin2" -eq 0 ]; then
-			hh=$(("$hh" + 1 ))
+		#Checking Triplet Combination
+		if [ "$coin1" -eq 0 -a "$coin2" -eq 0 -a "$coin3" -eq 0 ]; then
+			hhh=$(("$hhh" + 1 ))
 
-		elif [ "$coin1" -eq 0 -a "$coin2" -eq 1 ]; then
-                        ht=$(("$ht" + 1 ))
+		elif [ "$coin1" -eq 0 -a "$coin2" -eq 0 -a "$coin3" -eq 1 ]; then
+                        hht=$(("$hht" + 1 ))
 
-		elif [ "$coin1" -eq 1 -a "$coin2" -eq 0 ]; then
-                        th=$(("$th" + 1 ))
+		elif [ "$coin1" -eq 0 -a "$coin2" -eq 1 -a "$coin3" -eq 1 ]; then
+                        htt=$(("$htt" + 1 ))
 
-		elif [ "$coin1" -eq 1 -a "$coin2" -eq 1 ]; then
-                        tt=$(("$tt" + 1 ))
+		elif [ "$coin1" -eq 0 -a "$coin2" -eq 1 -a "$coin3" -eq 0 ]; then
+                        hth=$(("$hth" + 1 ))
+
+		elif [ "$coin1" -eq 1 -a "$coin2" -eq 0 -a "$coin3" -eq 0 ]; then
+                        thh=$(("$thh" + 1 ))
+
+                elif [ "$coin1" -eq 1 -a "$coin2" -eq 0 -a "$coin3" -eq 1 ]; then
+                        tht=$(("$tht" + 1 ))
+
+                elif [ "$coin1" -eq 1 -a "$coin2" -eq 1 -a "$coin3" -eq 0 ]; then
+                        tth=$(("$tth" + 1 ))
+
+                elif [ "$coin1" -eq 1 -a "$coin2" -eq 1 -a "$coin3" -eq 1 ]; then
+                        ttt=$(("$ttt" + 1 ))
+
 		fi
 	done
+
 	#Store Into Dictionary
-	coin[hh]=$hh
-	coin[ht]=$ht
-	coin[th]=$th
-        coin[tt]=$tt
+	coin[hhh]=$hhh
+	coin[hht]=$hht
+	coin[htt]=$htt
+        coin[hth]=$hth
+	coin[thh]=$thh
+        coin[tht]=$tht
+        coin[tth]=$tth
+        coin[ttt]=$ttt
 
-	#Displaying results
-	echo "Out of $number Times"
-	for j in ${!coin[@]}
+
+	#Counting Percentage and Displaying results
+	echo "Flipping Coins $number Times"
+	for k in ${!coin[@]}
 	do
-		echo "$j -- ${coin[$j]}"
+		display[$k]=`awk -v number=$number -v a=${coin[$k]} 'BEGIN{result=(100 * a  / number); print result}'`
+		echo "$k : ${display[$k]} %"
 	done
-
-	#Counting Percentage and Display
-	hh_Per=`awk -v number=$number -v hh=${coin[hh]} 'BEGIN{result=(100 * hh  / number); print result}'`
-	ht_Per=`awk -v number=$number -v ht=${coin[ht]} 'BEGIN{result=(100 * ht  / number); print result}'`
-	th_Per=`awk -v number=$number -v th=${coin[th]} 'BEGIN{result=(100 * th  / number); print result}'`
-        tt_Per=`awk -v number=$number -v tt=${coin[tt]} 'BEGIN{result=(100 * tt  / number); print result}'`
-
-	echo "Percentage Display"
-	echo "hh : $hh_Per %, ht : $ht_Per %, th : $th_Per %, tt : $tt_Per %"
 else
 	echo "Wrong Input !!!"
 fi
