26b374558f1cf8929ecbeabff760d28b5e4ccb4f
AkashSuchak <suchak.akash@gmail.com>
2020-10-18 14:52:23 +0530

    Added : Doublet Combinations and storing values in Dictionary and Displaying Percentage wise

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -12,8 +12,10 @@ read -p "Enter Number : " number
 #Declaring Dictionary
 declare -A coin
 
-totalHead=0
-totalTail=0
+hh=0
+ht=0
+th=0
+tt=0
 
 if [[ $number ]] && [ "$number" -eq "$number" 2>/dev/null -a  "$number" -gt 0 ]; then
 
@@ -21,28 +23,44 @@ if [[ $number ]] && [ "$number" -eq "$number" 2>/dev/null -a  "$number" -gt 0 ];
 	for (( i=1; i<="$number"; i++ ))
 	do
 		#Random Value 0 or 1
-		isCheck=$((RANDOM%2))
+		coin1=$((RANDOM%2))
+		coin2=$((RANDOM%2))
 
-		#Checking Heads or Tails
-		if [ "$isCheck" -eq 0 ]; then
-			totalHead=$(("$totalHead" + 1 ))
-		else
-			totalTail=$(("$totalTail" + 1 ))
+		#Checking Doublet
+		if [ "$coin1" -eq 0 -a "$coin2" -eq 0 ]; then
+			hh=$(("$hh" + 1 ))
+
+		elif [ "$coin1" -eq 0 -a "$coin2" -eq 1 ]; then
+                        ht=$(("$ht" + 1 ))
+
+		elif [ "$coin1" -eq 1 -a "$coin2" -eq 0 ]; then
+                        th=$(("$th" + 1 ))
+
+		elif [ "$coin1" -eq 1 -a "$coin2" -eq 1 ]; then
+                        tt=$(("$tt" + 1 ))
 		fi
 	done
 	#Store Into Dictionary
-	coin[h]=$totalHead
-	coin[t]=$totalTail
+	coin[hh]=$hh
+	coin[ht]=$ht
+	coin[th]=$th
+        coin[tt]=$tt
 
 	#Displaying results
 	echo "Out of $number Times"
-	echo "Heads : ${coin[h]} -- Tails : ${coin[t]}"
+	for j in ${!coin[@]}
+	do
+		echo "$j -- ${coin[$j]}"
+	done
 
 	#Counting Percentage and Display
-	headPer=`awk -v number=$number -v totalHead=${coin[h]} 'BEGIN{result=(100 * totalHead  / number); print result}'`
-	tailPer=`awk -v number=$number -v totalTail=${coin[t]} 'BEGIN{result=(100 * totalTail  / number); print result}'`
+	hh_Per=`awk -v number=$number -v hh=${coin[hh]} 'BEGIN{result=(100 * hh  / number); print result}'`
+	ht_Per=`awk -v number=$number -v ht=${coin[ht]} 'BEGIN{result=(100 * ht  / number); print result}'`
+	th_Per=`awk -v number=$number -v th=${coin[th]} 'BEGIN{result=(100 * th  / number); print result}'`
+        tt_Per=`awk -v number=$number -v tt=${coin[tt]} 'BEGIN{result=(100 * tt  / number); print result}'`
+
 	echo "Percentage Display"
-	echo "Heads : $headPer %  -- Tails : $tailPer %"
+	echo "hh : $hh_Per %, ht : $ht_Per %, th : $th_Per %, tt : $tt_Per %"
 else
 	echo "Wrong Input !!!"
 fi
